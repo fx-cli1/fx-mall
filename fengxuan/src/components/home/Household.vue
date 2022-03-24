460679cc9a416af973d52664e69a96aa0299398c
zhoup <3202323556@qq.com>
2022-03-24 20:33:35 +0800

    提交分类

--- fengxuan/src/components/home/Household.vue
+++ fengxuan/src/components/home/Household.vue
@@ -1,13 +1,62 @@
 <template>
-  <div>家电</div>
+  <div>
+    <myswipe
+      :imgsrclist="imgarr"
+      :autoplayFlag="active == '3' ? true : false"
+    ></myswipe>
+
+    
+  </div>
 </template>
 
 <script>
-export default {
+import myswipe from "./Myswipe.vue";
+import { mapState } from "vuex";
 
-}
+
+export default {
+  components:{
+    myswipe
+  },
+  computed: {
+    ...mapState("homestore", ["active"]),
+  },
+  data (){
+   return{//你的轮播图能跑吗  自动轮播没了
+      imgarr: [
+        {
+          id: 1,
+          imgsrc:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0b2bb13c396cc6205dd91da3a91a275a.jpg?thumb=1&w=720&h=360",
+        },
+        {
+          id: 2,
+          imgsrc:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37bd342303515c7a1a54681599e319a1.jpg?thumb=1&w=720&h=360",
+        },
+        {
+          id: 3,
+          imgsrc:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/bb4a8b7d9e77567c7330776a866e717f.jpg?thumb=1&w=720&h=360",
+        },
+        {
+          id: 4,
+          imgsrc:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/1ab1ffaaeb5ca4c02674e9f35b1fd17c.jpg?thumb=1&w=720&h=360",
+        },
+        {
+          id: 5,
+          imgsrc:
+            "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a2b3ab270e5ae4c9e85d6607cdb97008.jpg?thumb=1&w=720&h=360",
+        },
+      ],
+   }
+
+  }
+};
 </script>
 
-<style>
 
+<style>
+ 
 </style>
--- fengxuan/src/views/Classification.vue
+++ fengxuan/src/views/Classification.vue
@@ -1,16 +1,43 @@
 <template>
   <div class="about">
     <h1>分类</h1>
+    <van-tree-select height="55vw" :items="items" :main-active-index.sync="active">
+      <template #content>
+        <content v-if="active === 0">
+          <div class="one">
+            <div class="one-1">
+              <van-image src="https://img01.yzcdn.cn/vant/apple-1.jpg" />
+            </div>
+            <div class="one-1">
+              <van-image src="https://img01.yzcdn.cn/vant/apple-2.jpg" />
+            </div>
+            <div class="one-1">
+              <van-image src="https://img01.yzcdn.cn/vant/apple-3.jpg" />
+            </div>
+          </div>
+        </content>
+        <!-- <van-image v-if="active === 0" src="https://img01.yzcdn.cn/vant/apple-1.jpg" /> -->
+        <van-image v-if="active === 1" src="https://img01.yzcdn.cn/vant/apple-2.jpg" />
+        <van-image v-if="active === 2" src="https://img01.yzcdn.cn/vant/apple-3.jpg" />
+      </template>
+    </van-tree-select>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
-  },
+    return {
+      active: 0,
+      items: [{ text: "分组 1" }, { text: "分组 2" }, { text: "3" }]
+    };
+  }
 };
 </script>
 
 <style>
+.one {
+  display: flex;
+  
+}
 </style>
