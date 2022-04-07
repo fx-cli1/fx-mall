cb8f8ba411ededee0e901a51f6dd9814551bed99
unknown <wbhuym@centaline.com.cn>
2022-04-07 11:32:04 +0800

    添加后端接口

---
+++ server/src/main.ts
@@ -0,0 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('峰选商城')
+    .setDescription('峰选商城接口文档')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.useGlobalPipes(new ValidationPipe);
+  await app.listen(9080);
+}
+bootstrap();
