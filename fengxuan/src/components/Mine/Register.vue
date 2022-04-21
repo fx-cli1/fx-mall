2b20d12c7150aafba18d1438b9ca11edadc1d35b
unknown <wbhuym@centaline.com.cn>
2022-04-21 10:51:09 +0800

    配置代理

--- fengxuan/src/components/Mine/Register.vue
+++ fengxuan/src/components/Mine/Register.vue
@@ -1,57 +1,70 @@
 <template>
   <div>
     <van-nav-bar
-  title="注册"
-  left-text="返回"
-  left-arrow
-  @click-left="onClickLeft"
-/>
+      title="注册"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+    />
     <van-form @submit="onSubmit">
-  <van-cell-group inset>
-    <van-field
-      v-model="username"
-      name="用户名"
-      label="用户名"
-      placeholder="用户名"
-      :rules="[{ required: true, message: '请填写用户名' }]"
-    />
-    <van-field
-      v-model="password"
-      type="password"
-      name="密码"
-      label="密码"
-      placeholder="密码"
-      :rules="[{ required: true, message: '请填写密码' }]"
-    />
-  </van-cell-group>
-  <div style="margin: 16px;">
-    <van-button round block type="primary" native-type="submit">
-      提交
-    </van-button>
-  </div>
-</van-form>
+      <van-cell-group inset>
+        <van-field
+          v-model="form.username"
+          name="用户名"
+          label="用户名"
+          placeholder="用户名"
+          :rules="[{ required: true, message: '请填写用户名' }]"
+        />
+        <van-field
+          v-model="form.password"
+          type="password"
+          name="密码"
+          label="密码"
+          placeholder="密码"
+          :rules="[{ required: true, message: '请填写密码' }]"
+        />
+      </van-cell-group>
+      <div style="margin: 16px">
+        <van-button round block type="primary" native-type="submit">
+          提交
+        </van-button>
+      </div>
+    </van-form>
   </div>
 </template>
 
 <script>
+import userModel from "../../model/users/index";
 export default {
-  data(){
+  data() {
     return {
-     username:"",
-     password:"",
-    }
-  },
-  methods:{
-      onSubmit(values){
-       console.log(values)
+      form: {
+        username: "",
+        password: "",
       },
-    onClickLeft(){
-       this.$router.go(-1)
-    }
-  }
-}
+    };
+  },
+  methods: {
+    async onSubmit() {
+      // console.log(this.form);
+      // let res = await userModel.register({
+      //   user_name: this.form.username,
+      //   password: this.form.password,
+      // });
+      // console.log(res);
+      userModel.register({
+        user_name: this.form.username,
+        password: this.form.password,
+      }).then(()=>{
+        
+      });
+    },
+    onClickLeft() {
+      this.$router.go(-1);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
