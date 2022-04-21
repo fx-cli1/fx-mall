e0c6bf3edb8909ab6a3b45337b2c1744e1f85444
qiutian <2680815407@qq.com>
2022-04-21 12:08:30 +0800

    修改wyy代理配置

--- fengxuan/src/model/wyy-mmyz/index.js
+++ fengxuan/src/model/wyy-mmyz/index.js
@@ -1,13 +1,13 @@
 import axios from '../axios';
 export default {
-    sendYzm:(params)=>{
+    sendYzm:(params)=>{  //发送短信
         return axios({
             method:'get',
             url:'/wyy-api/captcha/sent',
             params
         })
     },
-    verificationYzm:(params)=>{
+    verificationYzm:(params)=>{   //验证短信
         return axios({
             method:'get',
             url:'/wyy-api/captcha/verify',
