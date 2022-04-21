53ab552eaddb41a732c1005fdfcc30c2f65ee37e
unknown <wbhuym@centaline.com.cn>
2022-04-21 16:14:55 +0800

    修改登录注册接口

--- server/src/server/user/user.controller.ts
+++ server/src/server/user/user.controller.ts
@@ -7,15 +7,17 @@ import {
   Post,
   Put
 } from '@nestjs/common';
-import { CreateUserDTO, EditUserDTO } from './user.dto';
+import { CreateUserDTO, EditUserDTO, LoginUserDTO } from './user.dto';
 import { User } from './user.interface';
 import { UserService } from './user.service';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import  jwtModel from '../../jwt/index';
 
 interface UserResponse<T = unknown> {
   code: number;
   data?: T;
   message: string;
+  token?: string;
 }
 @ApiTags('账号操作')
 @Controller('user')
@@ -48,11 +50,49 @@ export class UserController {
   @ApiOperation({ summary: '注册新用户' })
   @Post('add')
   async addOne(@Body() body: CreateUserDTO): Promise<UserResponse> {
-    await this.userService.addOne(body);
-    return {
-      code: 200,
-      message: 'Success.'
-    };
+    let username = body.username;
+    let phone = body.phone;
+    let res = await this.userService.conditionFind({ username });
+    if (res && res.length > 0) {
+      return {
+        code: 200,
+        message: `${username}已被注册`,
+      }
+    } else {
+      let res1 = await this.userService.conditionFind({ phone });
+      if (res1 && res1.length > 0) {
+        return {
+          code: 200,
+          message: `${phone}已被注册`,
+        }
+      } else {
+        await this.userService.addOne(body);
+        return {
+          code: 200,
+          message: '注册成功'
+        };
+      }
+    }
+  }
+
+  @ApiOperation({ summary: '登录' })
+  @Post('login')
+  async login(@Body() body: LoginUserDTO): Promise<UserResponse> {
+    let username = body.username;
+    let password = body.password;
+    let res = await this.userService.conditionFind({ username, password });
+    if (res && res.length > 0) {
+      return {
+        code: 200,
+        message: '登录成功',
+        token:jwtModel.jwtSign(body),
+      }
+    } else {
+      return {
+        code: 200,
+        message: '登录失败，账号与密码不匹配'
+      }
+    }
   }
 
   // PUT /user/:_id
