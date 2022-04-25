eb2cb0b4b8e86e316c1a716f10ee3bbf9670f270
unknown <wbhuym@centaline.com.cn>
2022-04-25 11:07:51 +0800

    master

--- server/src/jwt/index.ts
+++ server/src/jwt/index.ts
@@ -3,23 +3,23 @@ const jwt = require('jsonwebtoken');
 
 
 const jwtSign = (data) => {
-    const token = jwt.sign(data, 'fengxuan', { expiresIn: 60 * 60})
+    const token = jwt.sign(data, 'fengxuan', { expiresIn: 60 * 60 })
     return token;
 }
 
 
-const jwtCheck = (token:string):boolean => {
+const jwtCheck = (token: string): any => {
     //前端headers传来的token值:
-  return  jwt.verify(token, 'fengxuan', (err, data) => {
+    return jwt.verify(token, 'fengxuan', (err, data) => {
         if (err) {
             return false;
         } else {
-            return true;
+            return data;
         }
     })
 }
 
-export default{
+export default {
     jwtSign,
     jwtCheck
 }
