623f3fde01d3b37dfbc4d98b460815cf1fee4563
qiutian <2680815407@qq.com>
2022-04-28 13:44:20 +0800

    提交

--- fengxuan/src/router/index.js
+++ fengxuan/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
-
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      NavigationFlag: true
+    }
   },
   {
     path: '/classification',
@@ -17,42 +17,118 @@ const routes = [
   }, {
     path: '/find',
     name: 'find',
-    component: () => import('../views/Find.vue')
+    component: () => import('../views/Find.vue'),
+    meta: {
+      NavigationFlag: true
+    }
   },
   {
     path: '/shoppingcart',
     name: 'shoppingcart',
-    component: () => import('../views/Shoppingcart.vue')
+    component: () => import('../views/Shoppingcart.vue'),
+    meta: {
+      NavigationFlag: false
+    }
   },
   {
     path: '/mine',
     name: 'mine',
-    component: () => import('../views/Mine.vue')
+    component: () => import('../views/Mine.vue'),
+    meta: {
+      NavigationFlag: true
+    }
   },
   {
     path: '/login',
     name: 'login',
-    component: () => import('../components/Mine/Login.vue')
+    component: () => import('../components/Mine/Login.vue'),
+    meta: {
+      NavigationFlag: false
+    }
   },
   {
     path: '/register',
     name: 'register',
-    component: () => import('../components/Mine/Register.vue')
+    component: () => import('../components/Mine/Register.vue'),
+    meta: {
+      NavigationFlag: false
+    }
   },
   {
-    path:'/exit',
-    name:'exit',
-    component:()=>import('../components/Mine/Exit.vue')
+    path: '/exit',
+    name: 'exit',
+    component: () => import('../components/Mine/Exit.vue'),
+    meta: {
+      NavigationFlag: false
+    }
   },
   {
-    path:'/detail',
-    name:'detail',
-    component:()=>import('../views/Detail.vue')
+    path: '/detail/:id',
+    name: 'detail',
+    component: () => import('../views/Detail.vue'),
+    beforeEnter: (to, from, next) => {//路由独享守卫，详情页只能从指定页面进入
+      if (from.path == "/" || from.path == "/shoppingcart") {
+        next()
+      } else {
+        next("/mine")
+      }
+    },
+    meta: {
+      NavigationFlag: false
+    }
+  },
+  {
+    path: '/changePwd',
+    name: 'changePwd',
+    component: () => import('../components/Exit/changePwd.vue'),
+    meta: {
+      NavigationFlag: false
+    }
+  },
+  {
+    path: '/location',
+    name: 'location',
+    component: () => import('../components/Exit/Location.vue'),
+    meta: {
+      NavigationFlag: false
+    }
+  },
+  {
+    path: '/location2',
+    name: 'location2',
+    component: () => import('../components/Exit/Location2.vue'),
+    meta: {
+      NavigationFlag: false
+    }
+  },
+  {
+    path: "/404",
+    component: () => import('../views/errorView.vue'),
+    meta: {
+      title: 404,
+      NavigationFlag: false
+    }
+  },
+  {
+    path: "*",//任意路径
+    redirect: "/404"//
   }
 ]
 
 const router = new VueRouter({
   routes
 })
+
+Vue.use(VueRouter)
+const originalPush = VueRouter.prototype.push
+// 修改 原型对象中的push方法
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
+// router.beforeEach((to, from, next) => {
+//   console.log(to, from);
+//   next()
+// })
 
 export default router
--- fengxuan/vue.config.js
+++ fengxuan/vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
                 }
             },
             '/upload-api': {
-                target: 'http://localhost:9080',   // 你本地不要用这个，本地用上面那个
+                target: 'http://localhost:9080',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/upload-api': ''
